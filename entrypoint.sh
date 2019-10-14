0672905b7dba63dc088854c23c0e06c383300ad5
Ezequiel Leites <ezequiel@leites.dev>
2019-10-14 18:01:37 +0200

    Integrate AWS in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,18 @@ then
   exit 1
 fi
 
-FOLDER=build
-
-# Installs Git
+echo "Installing tools.." && \
 apt-get update && \
 apt-get install -y git && \
+apt-get install supervisor python3-pip supervisor vim -y && \
+apt-get update -y && \
+pip3 install --upgrade --user awscli && \
+
+echo "Export path" && \
+export PATH=~/.local/bin:$PATH && \
 
 # Directs the action to the the Github workspace.
+echo "Moving to ${GITHUB_WORKSPACE}.." && \
 cd $GITHUB_WORKSPACE && \
 
 # Clone repository
@@ -23,10 +28,39 @@ git clone https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${G
 # Checks out to master.
 git checkout master && \
 
-# Install dependencies
-echo "Installing dependencies.." && \
-eval "yarn install" && \
+if [ -z "$BUILD_SCRIPTS" ]
+then
+  # Install dependencies
+  echo "Installing dependencies.." && \
+  eval "yarn install" && \
 
-# Builds the project
-echo "Running build scripts.." && \
-eval "yarn build"
+  # Builds the project
+  echo "Running build scripts.." && \
+  eval "yarn build"
+else
+  # Running commands
+  echo "Running build scripts.." && \
+  eval ${BUILD_SCRIPTS}
+fi
+
+echo "Where is aws?" && \
+eval which aws && \
+
+### echo "Reload bash profile" && \
+### . ~/.bash_profile && \
+
+echo "Get PATH variable" && \
+echo $PATH && \
+
+echo "Get AWS Cli version" && \
+aws --version && \
+
+# Deploy to S3
+echo "Run set region..." && \
+aws configure set region ${AWS_REGION} && \
+echo "Run set access key..." && \
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID} && \
+echo "Run set secret key..." && \
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY} && \
+echo "Run sync..." && \
+aws s3 sync ${FOLDER} s3://${BUCKET} --acl public-read
